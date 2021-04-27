2401ec9c8d9e8706ddf6bd62774b196b7bcee863
J-Sigma <jsasser143801@gmail.com>
2021-04-27 11:13:37 -0500

    Added Servlet and ConnectionFactory

---
+++ src/main/java/com/revature/utils/ConnectionFactory.java
@@ -0,0 +1,22 @@
+package com.revature.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+    private static final String URL = "";
+    private static final String USERNAME = "";
+    private static final String PASSWORD = "";
+
+    private static Connection conn;
+
+    public static Connection getConnection() {
+        try {
+            conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch(SQLException e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
+}
